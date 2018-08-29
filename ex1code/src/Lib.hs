c0e13b98bab06e503da112fbcba194fa1f2cf7b3
Odin Ugedal <odin@ugedal.com>
2018-08-29 13:31:13 +0200

    Fix all the things
    Almost

--- ex1code/src/Lib.hs
+++ ex1code/src/Lib.hs
@@ -1,26 +1,25 @@
 {-# LANGUAGE ParallelListComp #-}
 
 module Lib
-    ( add
-    , isVowel
-    , Status(..)
-    , amountOf
-    , fib
-    , ending
-    , takeInt
-    , fizzbuzz
-    , printFizz
-    , listOfEven
-    , zipped
-    , cartesian
-    ) where
+  ( add
+  , isVowel
+  , Status(..)
+  , amountOf
+  , fib
+  , ending
+  , takeInt
+  , fizzbuzz
+  , printFizz
+  , listOfEven
+  , zipped
+  , cartesian
+  ) where
 
-import Control.Monad (mapM_)
-import Prelude hiding (take)
+import           Control.Monad (mapM_)
+import           Prelude       hiding (take)
 
 -- TASK 1
 -- Simple functions
-
 -- finish the function "add" that takes two integers
 -- and returns the sum of them
 add :: Int -> Int -> Int
@@ -32,9 +31,14 @@ add n m = n + m
 -- hint: a string is a list
 -- hint2: use `elem` from Prelude
 isVowel :: Char -> Bool
-isVowel chr = chr `elem` (['a'..'z'] ++ ['A'..'Z'])
+isVowel chr = chr `elem` (['a' .. 'z'] ++ ['A' .. 'Z'])
 
-data Status = One | Two | Three | None deriving (Show, Eq)
+data Status
+  = One
+  | Two
+  | Three
+  | None
+  deriving (Show, Eq)
 
 -- complete the function "amountOf" which takes
 -- a name and a list of subjects
@@ -45,9 +49,8 @@ amountOf :: String -> [a] -> (Status, String)
 amountOf = undefined
 -- TASK 2
 -- Recursion
-
 -- finish the function "fib" that calculates the
--- nth fibonacci number 
+-- nth fibonacci number
 -- assuming that 0th = 0 and 1st = 1
 -- do not optimize it
 fib :: Int -> Int
@@ -57,7 +60,6 @@ fib n = fib(n-1) + fib(n-2)
 
 -- TASK 3
 -- Working with lists
-
 -- complete the function "ing" that takes a list of
 -- strings and returns them with the ending -ing
 -- if the string is empty, remove it from the list
@@ -66,7 +68,7 @@ ending = undefined
 
 -- complete the function "takeInt" that
 -- an integer n and a list of integers and
--- returns the first n elements of 
+-- returns the first n elements of
 -- the list
 takeInt :: Int -> [Int] -> [Int]
 takeInt = undefined
@@ -80,23 +82,22 @@ printFizz = mapM_ putStrLn fizzbuzz
 
 -- TASK 4
 -- List comprehensions
-
--- create a list "listOfEven" which contains all 
+-- create a list "listOfEven" which contains all
 -- even numbers that are equal or greater than 0
 -- use a list comprehension
 listOfEven :: [Integer]
-listOfEven = undefined
+listOfEven = [2 * num | num <- [0 ..]]
 
 -- create a list of tuples, "zipped"
 -- where each tuple contains the nth entry
 -- in the lists [1..26] and ['a'..'z']
 -- hint: parallel list comprehension
 zipped :: [(Int, Char)]
-zipped = undefined
+zipped = [(num, char) | num <- [1 .. 26] | char <- ['a' .. 'z']]
 
 -- create a list that contains the cartesian
 -- product of the two vectors [4, 6, 8]
 -- and [3, 7, 9]
 -- use a list comprehension
 cartesian :: [(Int, Int)]
-cartesian = undefined
+cartesian = [(i, j) | i <- [4, 6, 8], j <- [3, 7, 9]]
