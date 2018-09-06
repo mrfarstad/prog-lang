0062ae432d1922b2db7ccad1113c5762591a4216
Odin Ugedal <odin@ugedal.com>
2018-09-06 21:26:05 +0200

    Do stuff

--- ex2code/src/Lib.hs
+++ ex2code/src/Lib.hs
@@ -1,36 +1,37 @@
 module Lib
-    ( f0
-    , f1
-    , f2
-    , take
-    , map
-    , iterate
-    , filterPos
-    , filterPosMany
-    , flip3
-    , Maybe(..)
-    , safeHeadList
-    , safeHead
-    ) where
+  ( f0
+  , f1
+  , f2
+  , take
+  , map
+  , iterate
+  , filterPos
+  , filterPosMany
+  , flip3
+  , Maybe(..)
+  , safeHeadList
+  , safeHead
+  , isPerfSq
+  , accuracy
+  ) where
 
-import Prelude hiding (map, take, iterate, sqrt, Maybe)
+import           Prelude hiding (Maybe, iterate, map, sqrt, take)
 
 -- TASK 1
 -- Parametric polymorphism
-
 -- Below are three type signatures. Can you implement them? We
 -- say a function or implementation /inhabits/ it's type
 -- (signature). How many other inhabitants do these types
 -- have? What if we fixed a = Int, does that change your
 -- previous answer?
 f0 :: a -> a
-f0 = undefined
+f0 a = a
 
 f1 :: a -> b -> a
-f1 = undefined
+f1 a b = a
 
 f2 :: a -> b -> b
-f2 = undefined
+f2 a b = b
 
 -- Rewrite the function "takeInt" from exercice 1 as "take" so
 -- that it accepts a list of any type. If you used the
@@ -38,7 +39,11 @@ f2 = undefined
 -- own implementation this time. Be sure to include a type
 -- signature. (Hint: If you already wrote takeInt, you won't
 -- have to change much.)
-take = undefined
+take :: Int -> [a] -> [a]
+take n li =
+  if n > length li
+    then li
+    else [li !! x | x <- [0 .. n - 1]]
 
 -- The function head :: [a] -> a which returns the first
 -- element of a list, is /partial/, meaning it will crash for
@@ -46,36 +51,38 @@ take = undefined
 -- /total/ function "safeHeadList :: [a] -> [a]" which either
 -- gives the head, or nothing. Can you implement it using take?
 safeHeadList :: [a] -> [a]
-safeHeadList = undefined
+safeHeadList = take 1
 
 -- The output of safeHeadList is either empty or a singleton,
 -- and thus using a list as output-type is a bit misleading. A
 -- better choice is Maybe (sometimes called Optional):
-data Maybe a = Some a | None deriving (Eq, Show)
+data Maybe a
+  = Some a
+  | None
+  deriving (Eq, Show)
 
 -- Implement 'safeHead', representing failure using None.
 safeHead :: [a] -> Maybe a
-safeHead = undefined
+safeHead []     = None
+safeHead (x:xs) = Some x
 
 -- TASK 2
 -- Higher order functions
-
 map :: (a -> b) -> [a] -> [b]
-map = undefined
+map func arr = [func e | e <- arr]
 
 iterate :: (a -> a) -> a -> [a]
-iterate = undefined
+iterate func element = element : iterate func (func element)
 
 -- TASK 3
 -- Currying and partial application
-
 -- complete the function filterPos
--- that takes a list and returns 
+-- that takes a list and returns
 -- a filtered list containing only positive
 -- integers (including zero)
 -- use partial application to achieve this
 filterPos :: [Int] -> [Int]
-filterPos = undefined
+filterPos = filter (>= 0)
 
 -- complete the function filterPosMany
 -- that takes a list of lists and returns
@@ -83,21 +90,35 @@ filterPos = undefined
 -- integers (including zero)
 -- hint: use filterPos and map
 filterPosMany :: [[Int]] -> [[Int]]
-filterPosMany = undefined
+filterPosMany l = map filterPos l
 
 flip3 :: (a -> b -> c -> d) -> c -> b -> a -> d
-flip3 = undefined
+flip3 func c b a = func a b c
 
 -- TASK 4
 -- Infinite lists
+newton :: Double -> Double -> Double
+newton x x0 = x0 - (x0 ^ 2 - x) / (2 * x0)
+
+genApp :: Double -> [Double]
+genApp x = iterate (newton x) x
+
+isInteger :: Double -> Bool
+isInteger x = x == fromInteger (round x)
+
+approx :: Double -> [Double] -> Double
+approx x (a:aa) =
+  if a - head aa < x
+    then head aa
+    else approx x aa
 
 isPerfSq :: Double -> Bool
-isPerfSq = undefined
+isPerfSq x = isInteger (approx 0.000000001 (genApp x))
 
 --uncomment when isPerfSqr is defined
---accuracy :: Int -> Bool
---accuracy x = take x generated == take x [x^2 | x <- [1..]]
---                where
---             zpd       = zip [1..] (map isPerfSq [1..])
---             f (x,y)   = y == True
---             generated = fst . unzip $ filter f zpd
+accuracy :: Int -> Bool
+accuracy x = take x generated == take x [x ^ 2 | x <- [1 ..]]
+  where
+    zpd = zip [1 ..] (map isPerfSq [1 ..])
+    f (x, y) = y == True
+    generated = fst . unzip $ filter f zpd
