73649f49fabe5d1abd57730d55c79cb5b1962b92
Martin Farstad <farstad2k@gmail.com>
2018-09-13 18:37:14 +0200

    Fix tree

--- ex3code/src/Tree.hs
+++ ex3code/src/Tree.hs
@@ -1,24 +1,41 @@
 module Tree
-    (Tree(..)
-    ) where
+  ( Tree(..)
+  ) where
 
 -- TASK 3.2
 -- Binary Trees
-
-data Tree a = Branch (Tree a) a (Tree a) | Leaf a
+data Tree a
+  = Branch (Tree a)
+           a
+           (Tree a)
+  | Leaf a
   deriving (Eq, Show)
 
 -- The Foldable instance might prove tricky to define, so
 -- defining the specific functions first may be easier!
 treeSum :: (Num a) => Tree a -> a
-treeSum = undefined
+treeSum (Leaf a)         = a
+-- Branch :: Tree a -> a -> Tree a -> Tree a
+treeSum (Branch t1 a t2) = a + treeSum t1 + treeSum t2
 
 treeConcat :: Tree String -> String
-treeConcat = undefined
+treeConcat (Leaf a)         = a
+treeConcat (Branch t1 a t2) = a ++ treeConcat t1 ++ treeConcat t2
 
 treeMaximum :: (Ord a) => Tree a -> a
-treeMaximum = undefined
+treeMaximum (Leaf a) = a
+treeMaximum (Branch t1 a t2)
+  | a >= t1max = a
+  | a < t1max = t1max
+  | a >= t2max = a
+  | a < t2max = t2max
+  where
+    t1max = treeMaximum t1
+    t2max = treeMaximum t2
 
 -- Write a Foldable instance for Tree.
-instance Foldable Tree where
-    foldr = undefined
+instance Foldable Tree
+-- foldr :: (a -> b -> b) -> b -> t a -> b
+                                           where
+  foldr op acc (Leaf a)         = op a acc
+  foldr op acc (Branch t1 a t2) = foldr op (op a $ foldr op acc (t1)) t2
