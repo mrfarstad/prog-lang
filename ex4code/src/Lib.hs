2e4860fbc602c8bf76e2005548ef30d4a1531b6e
Martin Farstad <farstad2k@gmail.com>
2018-09-19 12:45:06 +0200

    Fix interpretor

--- ex4code/src/Lib.hs
+++ ex4code/src/Lib.hs
@@ -18,22 +18,25 @@ takeWhile, dropWhile :: (a -> Bool) -> [a] -> [a]
 takeWhile _ [] = []
 takeWhile p (x:xs)
   | p x = x : takeWhile p xs
-  | otherwise = takeWhile p xs
+  | otherwise = []
 
 dropWhile _ [] = []
-dropWhile p (x:xs)
+dropWhile p lst@(x:xs)
   | p x = dropWhile p xs
-  | otherwise = x : dropWhile p xs
+  | otherwise = lst
 
 break :: (a -> Bool) -> [a] -> ([a], [a])
-break p = span (not . p)
+break p xs = (takeWhile (not . p) xs, dropWhile (not . p) xs)
 
---break p (x:xs) =
---  | p x = ([], xs)
---  | otherwise = (
---break p xs = foldl (\acc curr -> if p curr then else) ([],[]) xs
-splitOn :: Eq a => a -> [a] -> [[a]]
-splitOn = undefined
+splitOn :: (Eq a) => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn s xs
+  | length res > 0 = [res] ++ splitOn s tail
+  | otherwise = []
+  where
+    p = (== s)
+    lst = dropWhile p xs
+    (res, tail) = break p lst
 
 data Token
   = TokOp Op
@@ -49,13 +52,39 @@ data Op
   deriving (Show, Eq)
 
 lex :: String -> [String]
-lex = undefined
+lex = splitOn ' '
+
+isAllDigits :: String -> Bool
+isAllDigits (x:xs) =
+  isDigit x &&
+  if length xs > 0
+    then isAllDigits xs
+    else True
+
+makeToken :: String -> Token
+makeToken str
+  | str == "+" = TokOp Plus
+  | str == "-" = TokOp Minus
+  | str == "*" = TokOp Mult
+  | str == "/" = TokOp Div
+  | isAllDigits str = TokInt (read str :: Int)
+  | otherwise = TokErr
+  where
+    int = (read str :: Int)
 
 tokenize :: [String] -> [Token]
-tokenize = undefined
+tokenize = map makeToken
 
 interpret :: [Token] -> [Token]
-interpret = undefined
+interpret = foldl foldingFunction []
+  where
+    foldingFunction (TokInt x:TokInt y:ys) (TokOp Plus) = (TokInt $ x + y) : ys
+    foldingFunction (TokInt x:TokInt y:ys) (TokOp Minus) = (TokInt $ y - x) : ys
+    foldingFunction (TokInt x:TokInt y:ys) (TokOp Mult) = (TokInt $ x * y) : ys
+    foldingFunction (TokInt x:TokInt y:ys) (TokOp Div) =
+      (TokInt $ y `div` x) : ys
+    foldingFunction xs (TokInt int) = TokInt int : xs
+    foldingFunction xs (TokErr) = [TokErr]
 
 opLeq :: Token -> Token -> Bool
 opLeq = undefined
